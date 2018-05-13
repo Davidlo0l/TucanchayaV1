894dac61c111a9c43a1d63034996c7c0c28a53a8
Davidlo0l <davidlo0l95@gmail.com>
2018-05-12 21:28:05 -0500

    Initial commit

---
+++ TucanchayaV1/TucanchayaV1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TucanchayaV1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+      
+           
+			InitializeComponent();
+            Btningresar.Clicked += Btningresar_Clicked;
+        }
+
+        private  async void Btningresar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Menu ());
+        }
+    }
+}
---
+++ TucanchayaV1/TucanchayaV1/MasterPageItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TucanchayaV1
+{
+    public class MasterPageItem
+    {
+        public string Cancha { get; set; }
+        public string Puntos { get; set; }
+        public string Historial { get; set; }
+        public Type Pagina { get; set; }
+    }
+}
